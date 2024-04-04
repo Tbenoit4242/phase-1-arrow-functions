db30d76368218e95adc030b96a98547de63cf10e
Trevor Benoit <Trevorb042@gmail.com>
2024-04-04 13:12:22 -0600

    complete

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (a,b) => {
+    a = 2000
+    b = 100
+    return a / b
+}
+const square = (x,y) =>{
+    x = 2
+    y = 2
+    return x * y
+}
+const add = (a,b) =>{
+    a = 3
+    b = 4
+    return a + b
+}
